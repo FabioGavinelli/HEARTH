df90a8aabf4271268378ea3cfbbb57a4d6648f8c
Marian Ilies <ilies.marianalexandru@gmail.com>
2019-01-19 16:22:06 +0100

    importate cose

---
+++ HEARTH/Assets/Scripts/Phase_2/RobotNavController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RobotNavController : MonoBehaviour {
+
+    
+    [SerializeField] public GameObject target;
+
+    private NavMeshAgent navMeshAgent;
+    private bool following = true;
+
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+
+    void Update()
+    {
+        //se il robot sta già inseguendo il player
+        if (following)
+        {
+            navMeshAgent.SetDestination(target.transform.position);
+            
+            //se ha raggiunto il target lo fa scomparire TOFIX
+            if (TargetReached())
+            {
+                //GAMEOVER TODO
+                Debug.Log("GAMEOVER");
+            }
+        }
+
+    }
+
+    //restituisce true se il target è raggiunto 
+    private bool TargetReached()
+    {
+        if (!navMeshAgent.pathPending)
+            if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+                if (!navMeshAgent.hasPath || navMeshAgent.velocity.sqrMagnitude == 0f)
+                    return true;
+
+        return false;
+    }
+
+}
