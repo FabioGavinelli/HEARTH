41d6a45351c3569e7db2be5d5e52889aa4c56f3f
Fabio Gavinelli <FabioGavinelli@users.noreply.github.com>
2019-02-12 22:59:56 +0100

    Aggiunti robot rotto che fanno luce

---
+++ HEARTH/Assets/ActivateRobotLight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateRobotLight : MonoBehaviour
+{
+    [SerializeField] private Light robotLight;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            robotLight.enabled = false;
+            Debug.Log("lumos!");
+            this.enabled = false;
+        }
+    }
+}
---
+++ HEARTH/Assets/Light_Flicker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Flicker : MonoBehaviour
+{
+    [SerializeField] private Light light;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+            StartCoroutine(Flick());
+    }
+
+    private IEnumerator Flick()
+    {
+            light.intensity = Random.Range(0.5f, 1.5f);
+            yield return null; 
+    }
+}
