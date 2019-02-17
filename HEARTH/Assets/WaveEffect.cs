4c6cb6dcd47b67c96bf6280fee372130be79cd5b
Fabio Gavinelli <FabioGavinelli@users.noreply.github.com>
2019-02-17 19:27:24 +0100

    Fase 2
    - aggiunto terrain

--- HEARTH/Assets/WaveEffect.cs
+++ HEARTH/Assets/WaveEffect.cs
@@ -7,18 +7,8 @@ public class WaveEffect : MonoBehaviour
 {
     private float angle = 1f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time, 3),  3.70f + Mathf.PingPong(Time.time, 1), 204f + Mathf.PingPong(Time.time, 0.5f));
-        //transform.rotation = new Quaternion(Mathf.PingPong(transform.eulerAngles.x, 15), transform.eulerAngles.y, transform.eulerAngles.z, 0);
-
-
+        transform.position = new Vector3(Mathf.PingPong(Time.time, 3),  3f + Mathf.PingPong(Time.time, 1), 204f + Mathf.PingPong(Time.time, 0.5f));     
     }
 }
