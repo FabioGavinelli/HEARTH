c13dc1748daf571e656b3f708c3915ddd19a6d20
Fabio Gavinelli <FabioGavinelli@users.noreply.github.com>
2019-02-18 12:52:55 +0100

    Fase 2
    - cancellato debug in uno script

--- HEARTH/Assets/Scripts/Phase_2/CollisionWithRobot.cs
+++ HEARTH/Assets/Scripts/Phase_2/CollisionWithRobot.cs
@@ -4,23 +4,11 @@ using UnityEngine;
 
 public class CollisionWithRobot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == "Robot")
         {
-            Debug.Log("COLLISION");
             this.GetComponent<Rigidbody>().AddForce(new Vector3(100f,100f,100f), ForceMode.Impulse);
             Destroy(this.transform.gameObject, 2f);
         }
